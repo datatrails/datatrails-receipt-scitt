7ea26b330084ceb0dc01f3a16959e43c433f4204
Robin Bryce <robin.bryce@rkvst.com>
2023-06-09 16:09:03 +0000

    feat: verification and decoding for simple hash receipts
    Output looks like this:
    
      {
        "anchor": "f29b54ec5f183ba17a434809bef8a06e457aa63cd99deab3bcb54715901f0910",
        "api_query": "https://app.dev-robin-0.dev.jitsuin.io/archivist/v2/assets/-/events?order_by=SIMPLEHASHV2&proof_mechanism=SIMPLE_HASH&timestamp_accepted_since=2023-06-01T13:33:00Z&timestamp_accepted_before=2023-06-08T11:18:02Z",
        "endOperator": "lt",
        "endTimeRFC3339": "2023-06-08T11:18:02Z",
        "endTimeUnix": 1686223082,
        "eventCount": 1,
        "hashSchemaVersion": 2,
        "proofMechanism": 2,
        "startOperator": "gte",
        "startTimeRFC3339": "2023-06-01T13:33:00Z",
        "startTimeUnix": 1685626380,
        "tenant": "tenant/c8c51acf-5084-48ba-9e76-2c3cd9439818"
      }

--- rkvst_receipt_scitt/khipureceipt.py
+++ rkvst_receipt_scitt/khipureceipt.py
@@ -1,13 +1,9 @@
 """
 RKVST Khipu event specifics
 """
-import uuid
-from datetime import datetime
-import rfc3339
 from eth_utils import to_checksum_address
 
 from . import trie_alg
-from .namedproofs import NamedProofs
 from .attribute_decoder import (
     decode_attribute_key,
     decode_attribute_value,
@@ -20,12 +16,7 @@ class KhipuReceiptMalformedAttributes(ValueError):
     The receipt encoding of the rkvst attributes is malformed
     """
 
-
-class KhipuReceiptMalformedValue(ValueError):
-    """
-    The receipt encoding of a storage value is not as expected
-    """
-
+from .receipt import bto3339, u256touuid, Receipt
 
 EXTRA_PARAMETERS = ["monotonic_version"]
 APPLICATION_PARAMETERS = trie_alg.APPLICATION_PARAMETERS + EXTRA_PARAMETERS
@@ -60,16 +51,6 @@ def _principal_from_rawstorage(rawstorage):
     }
 
 
-def _bto3339(b: bytes, scale=1):
-    """
-    convert a bytes array, interpreted as a big endian integer, to a utc timestamp RFC 3339
-    :param b: bytes
-    :param scale: the timestamp from the block chain has a consensus specific scale factor in the case of raft
-    """
-    unix = int.from_bytes(b, "big") / scale
-    return rfc3339.rfc3339(datetime.utcfromtimestamp(unix))
-
-
 def _whens_from_rawstorage(rawstorage):
     """
     :param rawstorage: the 3 element list of slot values from the 'when' proof
@@ -77,73 +58,18 @@ def _whens_from_rawstorage(rawstorage):
     # rkvst_simplehash.V1_FIELDS (in v1.py) defines constants for these dict
     # keys these in alignment with the public rkvst events api
     return {
-        "timestamp_declared": _bto3339(rawstorage[WHEN_DECLARED]),
-        "timestamp_accepted": _bto3339(rawstorage[WHEN_ACCEPTED]),
+        "timestamp_declared": bto3339(rawstorage[WHEN_DECLARED]),
+        "timestamp_accepted": bto3339(rawstorage[WHEN_ACCEPTED]),
         # scale down by 1000,000,000. raft block time stamps are in nanoseconds
         # and we need seconds to do the RFC 3339 conversion
-        "timestamp_committed": _bto3339(rawstorage[WHEN_COMMITTED], scale=1000000000),
+        "timestamp_committed": bto3339(rawstorage[WHEN_COMMITTED], scale=1000000000),
     }
 
-
-def _u256touuid(b: bytes) -> str:
+class KhipuReceipt(Receipt):
     """
-    convert a 32 byte value from khipu event storage to a uuid
+    This class adds khipu specific receipt handling. See :class:`Receipt` for a general description.
+
     """
-    if len(b) != 32:
-        raise KhipuReceiptMalformedValue(
-            f"expected 32 bytes for a uuid storage value not {len(b)}"
-        )
-
-    b = b[16:]  # the high bytes are zero
-
-    return uuid.UUID(int=int.from_bytes(b, "big"))
-
-
-class KhipuReceipt:
-    """
-    This class uses the EIP1186 *neutral* receipt format to encode a receipt for an RKVST 'khipu' event.
-
-    serviceparams and contents are as per draft-birkholz-scitt-receipts 2. "Common parameters" & 3. "Generic Receipt Structure".
-
-    But in essence the serviceparams identify the service and the appropriate interpretation of contents. Here,
-    our trie-alg is cEIP1186NamedProofs and the basic structure of the contents is:
-
-    .. code-block::
-
-        {
-          application_parameters: {
-             app_id: trusted service application identifier,
-             app_content_ref: trusted service application references,
-             element_manifest: [] the complete set of app-defined-names,
-                1:1 associative with named_proofs
-          },
-          block: hex-str block number the proof was read from
-          account: the contract account the proof was read from
-          named_proofs: [ list of named proofs, 1 per entry in element_manifest
-             {
-                name: app-defined-name
-                id: proof-element-id - one of the three trie alg intrinsics defined
-                    in elementmetadata.py or app specific defined by app_content_ref
-
-                ... one or more EIP 1186 merkle inclusion proofs and supporting
-                metadata
-             }
-          ]
-        ]
-
-    For serviceparams to be fully compliant we need at least two items here:
-    * a permanent service identifier (likely app.rkvst.io)
-    * the trie alg defining the format of contents, currently EIP1186NamedProofs
-
-    But the implementation simply assumes this for now.
-    """
-
-    def __init__(self, contents, serviceparams=None):
-        """
-        :param contents: this is the trie-alg "EIP1186NamedProofs" defined receipt contents
-        :param serviceparams: the service parameters required by draft-birkholz-scitt-receipts 2. "Common parameters"
-        """
-        self.namedproofs = NamedProofs(contents, serviceparams=serviceparams)
 
     def verify(self, worldroot: str = None):
         """Verify the named proofs
@@ -155,11 +81,7 @@ class KhipuReceipt:
 
         :param worldroot: ethereum world state root from the block header
         """
-        # TODO: pass in stateroot and timestamp so caller can provide it from block header
-        self.namedproofs.check_payload_keys()
-        self.namedproofs.check_application_parameters()
-        self.namedproofs.collect_proofs(*MANIFEST_ELEMENTS)
-        self.namedproofs.verify_proofs(worldroot)
+        super().verify(MANIFEST_ELEMENTS, worldroot)
 
     def decode(self):
         """decode the application values from the proof"""
@@ -203,8 +125,8 @@ class KhipuReceipt:
         creator = to_checksum_address(
             essentials.value(ESSENTIALS_CREATOR)
         )  # aka from address
-        eventUUID = _u256touuid(essentials.value(ESSENTIALS_KHIPUIDENTITY))
-        assetUUID = _u256touuid(essentials.value(ESSENTIALS_ASSETIDENTITY))
+        eventUUID = u256touuid(essentials.value(ESSENTIALS_KHIPUIDENTITY))
+        assetUUID = u256touuid(essentials.value(ESSENTIALS_ASSETIDENTITY))
 
         # TODO: missing the khipu schema version number 'monotonicversion'
         who_declared = self.namedproofs.decoded("who_declared")
---
+++ unittests/test_simplehashreceipt.py
@@ -0,0 +1,44 @@
+"""
+Test rkvst specific proof handling for khipu events
+"""
+
+import json
+
+from unittest import TestCase
+
+from rkvst_receipt_scitt.simplehashreceipt import SimpleHashReceipt
+
+from .datautils import json_loads_receipt_contents
+
+
+class TestSimpleHashReceipt(TestCase):
+    """TestSimpleHashReceipt"""
+
+    def test_decode_receipt(self):
+        """
+        test that we can verify and decode the receipt
+        """
+        contents = json_loads_receipt_contents("simplehash_receipt_happy_default.b64")
+        sr = SimpleHashReceipt(contents)
+        anchor = sr.decode()
+        self.assertEqual(anchor["tenant"], "tenant/c8c51acf-5084-48ba-9e76-2c3cd9439818")
+        self.assertEqual(anchor["anchor"], "f29b54ec5f183ba17a434809bef8a06e457aa63cd99deab3bcb54715901f0910")
+        self.assertEqual(anchor["hashSchemaVersion"], 2)
+        self.assertEqual(anchor["eventCount"], 1)
+        self.assertEqual(anchor["proofMechanism"], 2)
+        self.assertEqual(anchor["startTimeRFC3339"], '2023-06-01T13:33:00Z')
+        self.assertEqual(anchor["endTimeRFC3339"], '2023-06-08T11:18:02Z')
+        self.assertEqual(anchor["startTimeUnix"], 1685626380)
+        self.assertEqual(anchor["endTimeUnix"], 1686223082)
+        print(json.dumps(anchor, sort_keys=True, indent="  "))
+
+    def test_verify_receipt(self):
+        """
+        test that we can verify the receipt. this is really just a regression
+        test, its not a proper KAT.
+        """
+        contents = json_loads_receipt_contents("simplehash_receipt_happy_default.b64")
+        sr = SimpleHashReceipt(contents)
+        # Note: remember this is the public .stateRoot for simple hash (rather
+        # than .privateStateRoot as it is for khipu)
+        sr.verify("0x35ac20316361c00015ee4e8ebd0a023afa3b99ee5d47b82f201cfa5086c82034")
