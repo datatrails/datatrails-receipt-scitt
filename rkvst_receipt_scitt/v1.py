8950b10d761382e53d69bbd6ebaf86623660d16e
robinbryce <robinbryce@gmail.com>
2023-06-12 11:26:20 +0100

    feat: verification and decoding for simple hash receipts (#9)
    * feat: verification and decoding for simple hash receipts
    
    Output looks like this:
    
      {
        "anchor": "f29b54ec5f183ba17a434809bef8a06e457aa63cd99deab3bcb54715901f0910",
        "api_query": "https://app.dev-robin-0.dev.jitsuin.io/archivist/v2/assets/-/events?order_by=SIMPLEHASHV2&proof_mechanism=SIMPLE_HASH&timestamp_accepted_since=2023-06-01T13:33:00Z&timestamp_accepted_before=2023-06-08T11:18:02Z",
        "endOperator": "lt",
        "endTimeRFC3339": "2023-06-08T11:18:02Z",
        "endTimeUnix": 1686223082,
        "eventCount": 1,
        "hashSchemaVersion": 2,
        "proofMechanism": 2,
        "startOperator": "gte",
        "startTimeRFC3339": "2023-06-01T13:33:00Z",
        "startTimeUnix": 1685626380,
        "tenant": "tenant/c8c51acf-5084-48ba-9e76-2c3cd9439818"
      }
    
    * fix: minor review issues
    
    * fix: linter issues
    
    * fix: linter issues
    
    * fix: linter issues
    
    * fix: linter issues
    
    * fix: linter issues
    
    * fix: linter issues
    
    * fix: linter issues
    
    * fix: linter issues
    
    * fix: remove deploy stage from cicd workflow
    
    ---------
    
    Co-authored-by: Robin Bryce <robin.bryce@rkvst.com>

--- rkvst_receipt_scitt/v1.py
+++ rkvst_receipt_scitt/v1.py
@@ -4,6 +4,9 @@ import argparse
 import json
 from rkvst_receipt_scitt.receiptdecoder import receipt_trie_alg_contents
 from rkvst_receipt_scitt.khipureceipt import KhipuReceipt
+from rkvst_receipt_scitt.simplehashreceipt import SimpleHashReceipt
+
+SIMPLE_HASH_ELEMENT = "simplehash"
 
 
 def receipt_verify(opts):
@@ -12,11 +15,34 @@ def receipt_verify(opts):
     """
     b64 = opts.receipt.read()
     contents = json.loads(receipt_trie_alg_contents(b64)[1])
-    r = KhipuReceipt(contents)
+    r = load_receipt_contents(contents)
     r.verify(opts.worldroot)
     if opts.decode:
-        event = r.decode()
-        print(json.dumps(event, sort_keys=True, indent="  "))
+        decoded = r.decode()
+        if isinstance(r, SimpleHashReceipt):
+            decoded["api_query"] = api_query(decoded, opts.fqdn)
+
+        print(json.dumps(decoded, sort_keys=True, indent="  "))
+
+
+def load_receipt_contents(contents: dict):
+    """inspect the json decoded contents of a receipt and instantiate the appropriate verifier."""
+
+    if SIMPLE_HASH_ELEMENT in contents["application_parameters"]["element_manifest"]:
+        return SimpleHashReceipt(contents)
+
+    return KhipuReceipt(contents)
+
+
+def api_query(decoded: dict, fqdn: str):
+    """recover the simple hash api query from the anchor receipt decoded values"""
+
+    # Note: this MUST align with rkvst-simplehash-python's approach
+    path = f"https://{fqdn}/archivist/v2/assets/-/events"
+    path += "?order_by=SIMPLEHASHV2&proof_mechanism=SIMPLE_HASH"
+    path += f"&timestamp_accepted_since={decoded['startTimeRFC3339']}"
+    path += f"&timestamp_accepted_before={decoded['endTimeRFC3339']}"
+    return path
 
 
 def main(args=None):  # pragma: no cover
@@ -38,10 +64,15 @@ def main(args=None):  # pragma: no cover
         "-w",
         "--worldroot",
         help="""
-The storageroot for the etherum world state, required to verify the contract
+The storageroot for the ethereum world state, required to verify the contract
 account exists.  This value is obtained from archivist/v1/archivist/block. If
 not supplied the account existence is not verified.
 """,
+    )
+    s.add_argument(
+        "--fqdn",
+        help="The fully qualified domain name of the rkvst deployment",
+        default="app.rkvst.io",
     )
     s.add_argument(
         "receipt",
